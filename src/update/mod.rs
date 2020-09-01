6fe86b5689b7ea55d5ca1429c879743e73d6a080
Pearce Keesling <pearce.keesling@lifeomic.com>
2020-09-01 13:39:45 -0600

    cargo fix

--- src/update/mod.rs
+++ src/update/mod.rs
@@ -1,18 +1,16 @@
 use crate::{msg::Msg, state::{View, State}, api::get_posts};
 use flume::Sender;
-use futures::task::{Spawn, SpawnExt};
+use futures::{future::RemoteHandle, task::{Spawn, SpawnExt}};
 use anyhow::{Result, Error, bail};
 
 pub fn update(msg: Msg, state: &mut State, tx: Sender<Msg>, pool: impl Spawn) -> Result<()> {
     match msg {
         Msg::FetchSubreddit(sub) => {
-            pool.spawn(async move {
-                if let Err(e) = get_posts(sub.as_str()).await.and_then(|posts| {
-                    tx.send(Msg::SubredditResponse(posts)).map_err(Error::new)
-                }) {
-                    eprintln!("{:?}", e);
-                    tx.send(Msg::Error("Failed to fetch subreddit posts".to_owned()));
-                }
+            let _: RemoteHandle<Result<()>> = pool.spawn_with_handle(async move {
+                let posts = get_posts(sub.as_str()).await?;
+                dbg!(posts);
+                tx.send(Msg::SubredditResponse(posts)).map_err(Error::new)?;
+                Ok(())
             })?;
             state.view_state = View::Loading;
         },
